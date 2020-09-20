a48e7f3822e923bbb867fc48766db8a9d9763119
Jack Burgess <jackburgess124@gmail.com>
2020-09-20 20:27:53 +0100

    some stuff :shrug:

--- esp32-cam.ino
+++ esp32-cam.ino
@@ -99,6 +99,18 @@ void setup() {
     request->send(SPIFFS, FILE_PHOTO, "image/jpg", false);
   });
 
+  server.on("/picture", HTTP_GET, [](AsyncWebServerRequest * request) {
+    camera_fb_t * frame = NULL;
+    frame = esp_camera_fb_get();
+    request->send_P(
+      200,
+      "image/jpeg",
+      (const uint8_t *)frame->buf,
+      frame->len
+    );
+
+    esp_camera_fb_return(frame);
+  });
   // Start server
   server.begin();
 }
@@ -112,47 +124,47 @@ void loop() {
 }
 
 // Check if photo capture was successful
-bool checkPhoto(fs::FS &fs) {
+boolean checkPhoto(fs::FS &fs) {
   File f_pic = fs.open(FILE_PHOTO);
   unsigned int pic_size = f_pic.size();
   return (pic_size > 100);
 }
 
 // Capture Photo and Save it to SPIFFS
-void capturePhotoSaveSpiffs(void) {
+boolean capturePhotoSaveSpiffs(void) {
   camera_fb_t * fb = NULL; // pointer
-  bool ok = 0; // Boolean indicating if the picture has been taken correctly
+  boolean ok = false; // Boolean indicating if the picture has been taken correctly
 
-  do {
-    // Take a photo with the camera
-    Serial.println("Taking a photo...");
+  // Take a photo with the camera
+  Serial.println("Taking a photo...");
 
-    fb = esp_camera_fb_get();
-    if (!fb) {
-      Serial.println("Camera capture failed");
-      return;
-    }
+  fb = esp_camera_fb_get();
+  if (!fb || fb->len == 0) {
+    Serial.println("Camera capture failed");
+    return false;
+  }
 
-    // Photo file name
-    Serial.printf("Picture file name: %s\n", FILE_PHOTO);
-    File file = SPIFFS.open(FILE_PHOTO, FILE_WRITE);
+  // Photo file name
+  Serial.printf("Picture file name: %s\n", FILE_PHOTO);
+  File file = SPIFFS.open(FILE_PHOTO, FILE_WRITE);
 
-    // Insert the data in the photo file
-    if (!file) {
-      Serial.println("Failed to open file in writing mode");
-    } else {
-      file.write(fb->buf, fb->len); // payload (image), payload length
-      Serial.print("The picture has been saved in ");
-      Serial.print(FILE_PHOTO);
-      Serial.print(" - Size: ");
-      Serial.print(file.size());
-      Serial.println(" bytes");
-    }
-    // Close the file
-    file.close();
-    esp_camera_fb_return(fb);
+  // Insert the data in the photo file
+  if (!file) {
+    Serial.println("Failed to open file in writing mode");
+    return false;
+  } else {
+    file.write(fb->buf, fb->len); // payload (image), payload length
+    Serial.print("The picture has been saved in ");
+    Serial.print(FILE_PHOTO);
+    Serial.print(" - Size: ");
+    Serial.print(file.size());
+    Serial.println(" bytes");
+  }
+  // Close the file
+  file.close();
+  esp_camera_fb_return(fb);
 
-    // check if file has been correctly saved in SPIFFS
-    ok = checkPhoto(SPIFFS);
-  } while (!ok);
+  // check if file has been correctly saved in SPIFFS
+  ok = checkPhoto(SPIFFS);
+  return ok;
 }
--- index.h
+++ index.h
@@ -11,6 +11,7 @@ const char index_html[] PROGMEM = R"rawliteral(
 <body>
   <div id="container">
     <h2>ESP32-CAM Photo</h2>
+    <p id="error" style="display: none;">Error taking photo</p>
     <p>It might take more than 5 seconds to capture a photo.</p>
     <p>
       <button onclick="rotatePhoto();">ROTATE</button>
@@ -18,14 +19,20 @@ const char index_html[] PROGMEM = R"rawliteral(
       <button onclick="location.reload();">REFRESH PAGE</button>
     </p>
   </div>
-  <div><img src="saved-photo" id="photo" width="70%"></div>
+  <div><img src="/saved-photo" id="photo" width="70%"></div>
 </body>
 <script>
   var deg = 0;
   function capturePhoto() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "/capture", true);
-    xhr.send();
+    fetch("/capture")
+      .then((res) => {
+        if (res.status === 200) {
+          window.photo.src = "/saved-photo"
+          window.error.style = "display: none;"
+        } else {
+          window.error.style = "display: block;"
+        }
+      })
   }
   function rotatePhoto() {
     var img = document.getElementById("photo");
